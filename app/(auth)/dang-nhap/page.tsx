3419b1b0a87c8cb38269505a5a1cec8aea0a7f82
minhtran <trcongminh.261002@gmail.com>
2025-04-17 18:20:27 +0700

    feat: implement Register and Login pages with form validation; add layouts for authentication

--- app/(auth)/dang-nhap/page.tsx
+++ app/(auth)/dang-nhap/page.tsx
@@ -1,7 +1,140 @@
+"use client";
+
+import { Card, CardContent } from "@/components/ui/card";
+import Link from "next/link";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  EyeIcon,
+  EyeOffIcon,
+  LockIcon,
+  MailIcon,
+  UserIcon,
+} from "lucide-react";
+import type { Login as LoginType } from "@/@types/Auth";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [showPassword, setShowPassword] = React.useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginType>();
+
+  return (
+    <div className="w-full mt-10 flex items-center justify-center">
+      <Card className="rounded-md max-w-[600px] w-full shadow-none">
+        <CardContent>
+          <div className="flex border-b">
+            <Link
+              href="/dang-nhap"
+              className="w-1/2 text-center py-4 font-semibold text-xl text-gray-900 border-b-2 border-red-500"
+            >
+              Đăng nhập
+            </Link>
+            <Link
+              href="/dang-ky"
+              className="w-1/2 text-center py-4 font-semibold text-xl text-gray-500 border-b border-gray-200"
+            >
+              Tạo tài khoản mới
+            </Link>
+          </div>
+
+          <form className="mt-10 space-y-4">
+            <div className="relative">
+              <Input
+                className="rounded-2xl h-12 w-full pl-10 shadow-none"
+                placeholder="Email"
+                {...register("email", {
+                  required: "Email không được để trống",
+                  pattern: {
+                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    message: "Email không hợp lệ",
+                  },
+                })}
+              />
+              <MailIcon
+                className="absolute top-4 left-3 text-gray-400"
+                size={20}
+              />
+              {errors.email && (
+                <span className="text-red-500 text-sm">
+                  {errors.email.message}
+                </span>
+              )}
+            </div>
+
+            <div className="relative">
+              <Input
+                type={showPassword ? "text" : "password"}
+                className="rounded-2xl h-12 w-full pl-10 shadow-none"
+                placeholder="Mật khẩu"
+                {...register("password", {
+                  required: "Mật khẩu không được để trống",
+                  minLength: {
+                    value: 6,
+                    message: "Mật khẩu phải có ít nhất 6 ký tự",
+                  },
+                })}
+              />
+              <LockIcon
+                className="absolute top-4 left-3 text-gray-400"
+                size={20}
+              />
+
+              {showPassword ? (
+                <EyeIcon
+                  className="absolute top-4 right-3 text-gray-400 cursor-pointer"
+                  size={20}
+                  onClick={() => setShowPassword(!showPassword)}
+                />
+              ) : (
+                <EyeOffIcon
+                  className="absolute top-4 right-3 text-gray-400 cursor-pointer"
+                  size={20}
+                  onClick={() => setShowPassword(!showPassword)}
+                />
+              )}
+              {errors.password && (
+                <span className="text-red-500 text-sm">
+                  {errors.password.message}
+                </span>
+              )}
+            </div>
+
+            <Button
+              type="submit"
+              className="text-md cursor-pointer rounded-2xl h-12 w-full shadow-none bg-[#d61117] text-white hover:bg-[#d61117] focus:bg-[#d61117] focus:ring-2 focus:ring-[#d61117] focus:ring-offset-2"
+            >
+              Đăng nhập
+            </Button>
+          </form>
+
+          <div className="mt-4 text-center">
+            <Link
+              href="/quen-mat-khau"
+              className="text-md text-blue-500 hover:text-blue-700 hover:underline"
+            >
+              Quên mật khẩu?
+            </Link>
+          </div>
+
+          <div className="mt-10 text-center text-sm">
+            Qua việc đăng nhập hoặc tạo tài khoản, bạn đồng ý với các{" "}
+            <span className="text-blue-500">điều khoản sử dụng</span> cũng như{" "}
+            <span className="text-blue-500">chính sách bảo mật</span> của chúng
+            tôi
+          </div>
+
+          <div className="mt-2 text-center text-sm">
+            Bản quyền © 2015 - 2025 Phongtro123.com
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default LoginPage;
